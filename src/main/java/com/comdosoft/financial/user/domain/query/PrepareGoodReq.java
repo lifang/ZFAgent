ea18d2847558a1413c7964ea761a875076b3e809
jwb445875 <445875775@qq.com>
2015-02-11 19:35:58 +0800

    调货配货

---
+++ src/main/java/com/comdosoft/financial/user/domain/query/PrepareGoodReq.java
@@ -0,0 +1,128 @@
+package com.comdosoft.financial.user.domain.query;
+
+public class PrepareGoodReq {
+
+    private int id;
+    
+    private int customer_id;
+    
+    private int agents_id;
+
+    private int son_agents_id;
+    
+    private int good_id;
+
+    private int paychannel_id;
+    
+    private String[] serial_nums;
+
+    private String startTime;
+    
+    private String endTime;
+    
+    //--------
+    private int quantity;
+    
+    private String serial_num;
+    
+    private String terminal_list;
+    
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getGood_id() {
+        return good_id;
+    }
+
+    public void setGood_id(int good_id) {
+        this.good_id = good_id;
+    }
+
+    public int getPaychannel_id() {
+        return paychannel_id;
+    }
+
+    public void setPaychannel_id(int paychannel_id) {
+        this.paychannel_id = paychannel_id;
+    }
+
+ 
+
+    public String getTerminal_list() {
+        return terminal_list;
+    }
+
+    public void setTerminal_list(String terminal_list) {
+        this.terminal_list = terminal_list;
+    }
+
+    public String[] getSerial_nums() {
+        return serial_nums;
+    }
+
+    public void setSerial_nums(String[] serial_nums) {
+        this.serial_nums = serial_nums;
+    }
+
+    public String getSerial_num() {
+        return serial_num;
+    }
+
+    public void setSerial_num(String serial_num) {
+        this.serial_num = serial_num;
+    }
+
+    public int getSon_agents_id() {
+        return son_agents_id;
+    }
+
+    public void setSon_agents_id(int son_agents_id) {
+        this.son_agents_id = son_agents_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getAgents_id() {
+        return agents_id;
+    }
+
+    public void setAgents_id(int agents_id) {
+        this.agents_id = agents_id;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
--- src/main/java/com/comdosoft/financial/user/service/StockService.java
+++ src/main/java/com/comdosoft/financial/user/service/StockService.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.comdosoft.financial.user.domain.query.StockReq;
+import com.comdosoft.financial.user.mapper.zhangfu.GoodMapper;
 import com.comdosoft.financial.user.mapper.zhangfu.StockMapper;
+import com.comdosoft.financial.user.utils.SysUtils;
 
 
 @Service
@@ -19,12 +21,29 @@ public class StockService {
 
     @Autowired
     private StockMapper stockMapper;
+    
+    @Autowired
+    private GoodMapper goodMapper;
 
     public Map<String, Object> getList(StockReq req) {
         Map<String, Object> map=new HashMap<String, Object>();
         int total=stockMapper.getStockTotal(req);
         map.put("total", total);
         List<Map<String, Object>> list=stockMapper.getStockList(req);
+        if(total>0){
+            for (Map<String, Object> map2 : list) {
+                List<String> goodPics = goodMapper.getgoodPics(SysUtils.Object2int(map2.get("good_id")));
+                if (null != goodPics && goodPics.size() > 0) {
+                    map2.put("picurl", goodPics.get(0));
+                }
+                req.setGood_id(SysUtils.Object2int(map2.get("good_id")));
+                req.setPaychannel_id(SysUtils.Object2int(map2.get("paychannel_id")));
+                map2.put("hoitoryCount", stockMapper.getHoitoryCount(req));
+                map2.put("openCount", stockMapper.getOpenCount(req));
+                map2.put("agentCount", stockMapper.getAgentCount(req));
+                map2.put("totalCount", stockMapper.getTotalCount(req));
+            }
+        }
         map.put("list", list);
         return map;
     }

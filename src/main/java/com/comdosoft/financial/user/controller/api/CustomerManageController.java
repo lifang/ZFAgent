6bf3f982006f0211d25e8c97fed450c61165a9b1
yangyibin <565074463@qq.com>
2015-03-31 14:29:42 +0800

    用于APP调用的用户管理接口

--- src/main/java/com/comdosoft/financial/user/controller/api/CustomerManageController.java
+++ src/main/java/com/comdosoft/financial/user/controller/api/CustomerManageController.java
@@ -27,7 +27,11 @@ public class CustomerManageController {
 	@Autowired
 	private SystemSetService sys;
 	
-	
+	/**
+	 * 获取用户列表
+	 * @param req
+	 * @return
+	 */
 	@RequestMapping(value = "getList", method = RequestMethod.POST)
 	public Response getList(@RequestBody CustomerManageReq req){
 		Response response = new Response();
@@ -35,5 +39,62 @@ public class CustomerManageController {
         response.setCode(Response.SUCCESS_CODE);
         response.setResult(result);
         return response;
+	}
+	
+	/**
+	 * 新增用户
+	 * @param req
+	 * @return
+	 */
+	@RequestMapping(value = "insert", method = RequestMethod.POST)
+	public Response insert(@RequestBody CustomerManageReq req){
+		Response response=new Response();
+		Map<String,Object> result= customerManageService.insert(req);
+		response.setCode(Integer.parseInt(result.get("resultCode").toString()));
+		response.setMessage(result.get("resultInfo").toString());
+		return response;
+	}
+	
+	/**
+	 * 删除单个用户
+	 * @param req
+	 * @return
+	 */
+	@RequestMapping(value = "deleteOne", method = RequestMethod.POST)
+	public Response deleteOne(@RequestBody CustomerManageReq req){
+		Response response=new Response();
+		Map<String,Object> result= customerManageService.deleteOne(req);
+		response.setCode(Integer.parseInt(result.get("resultCode").toString()));
+		response.setMessage(result.get("resultInfo").toString());
+		return response;
+	}
+	
+	/**
+	 * 批量删除用户
+	 * @param req
+	 * @return
+	 */
+	@RequestMapping(value = "deleteAll", method = RequestMethod.POST)
+	public Response deleteAll(@RequestBody CustomerManageReq req){
+		Response response=new Response();
+		Map<String,Object> result= customerManageService.deleteAll(req);
+		response.setCode(Integer.parseInt(result.get("resultCode").toString()));
+		response.setMessage(result.get("resultInfo").toString());
+		return response;
+	}
+	
+	/**
+	 * 查看用户详细信息
+	 * @param req
+	 * @return
+	 */
+	@RequestMapping(value = "getInfo", method = RequestMethod.POST)
+	public Response getInfo(@RequestBody CustomerManageReq req){
+		Response response=new Response();
+		Map<String,Object> result= customerManageService.getInfo(req);
+		response.setCode(Integer.parseInt(result.get("resultCode").toString()));
+		response.setMessage(result.get("resultInfo").toString());
+		response.setResult(result.get("result"));
+		return response;
 	}
 }
--- src/main/java/com/comdosoft/financial/user/service/CustomerManageService.java
+++ src/main/java/com/comdosoft/financial/user/service/CustomerManageService.java
@@ -6,9 +6,16 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.comdosoft.financial.user.domain.Response;
 import com.comdosoft.financial.user.domain.query.CustomerManageReq;
+import com.comdosoft.financial.user.domain.query.LowerAgentReq;
+import com.comdosoft.financial.user.domain.zhangfu.Customer;
 import com.comdosoft.financial.user.mapper.zhangfu.CustomerManageMapper;
+import com.comdosoft.financial.user.mapper.zhangfu.LowerAgentMapper;
+import com.comdosoft.financial.user.utils.SysUtils;
 
 /**
  * 用于App用户管理
@@ -19,7 +26,10 @@ import com.comdosoft.financial.user.mapper.zhangfu.CustomerManageMapper;
 public class CustomerManageService {
 	@Autowired
 	private CustomerManageMapper customerManageMapper;
-	
+	@Autowired
+	private LowerAgentMapper lowerAgentMapper;
+	@Autowired
+	private LowerAgentReq lowerAgentReq;
 	@Autowired
 	private SystemSetService sys;
 	
@@ -31,4 +41,231 @@ public class CustomerManageService {
         map.put("list", list);
         return map;
     }
+	/**
+	 * 新增
+	 * @param req
+	 * @return
+	 */
+	@Transactional(value="transactionManager-zhangfu",propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public Map<String, Object> insert(CustomerManageReq req){
+		Map<String, Object> map=new HashMap<String, Object>();
+		int resultCode=Response.ERROR_CODE;
+		StringBuilder resultInfo=new StringBuilder();
+		//校验格式等 名字，密码，登陆ID不存在可用
+		if(req.getUserName().trim().equals("")){
+			resultInfo.setLength(0);
+			resultInfo.append("姓名不能为空");
+		}else{
+			if(req.getPwd().trim().equals("")){
+				resultInfo.setLength(0);
+				resultInfo.append("密码不能为空");
+			}else{
+				if(!req.getPwd().equals(req.getPwd1())){
+					resultInfo.setLength(0);
+					resultInfo.append("两次输入的密码不一致");
+				}else{
+					lowerAgentReq.setLoginId(req.getLoginId());
+					int temp=lowerAgentMapper.checkLoginId(lowerAgentReq);
+					if(temp>=1){
+						resultInfo.setLength(0);
+						resultInfo.append("该登陆ID已经存在");
+					}else{
+						//密码加密，执行存入数据库
+						req.setPwd(SysUtils.string2MD5(req.getPwd()));
+						//向customers表插入记录
+						lowerAgentReq.setAgentName(req.getUserName());
+						lowerAgentReq.setPwd(req.getPwd());
+						int temp1=lowerAgentMapper.addNewCustomer(lowerAgentReq);
+						if(temp1<1){
+							resultInfo.setLength(0);
+							resultInfo.append("新增customers表出错");
+						}else{
+							//更新关联关系表
+							int customerId=lowerAgentMapper.getCustomerId(lowerAgentReq);
+							req.setCustomerId(customerId);
+							//检查是否已经存在
+							int temp2=customerManageMapper.getCusAgentInfo(req);
+							if(temp2<1){
+								resultInfo.setLength(0);
+								resultInfo.append("已存在该用户与代理商的关联关系");
+							}else{
+								req.setTypes(2);
+								req.setStatus(1);
+								int temp3=customerManageMapper.creCusAgentRelation(req);
+								if(temp3<1){
+									resultInfo.setLength(0);
+									resultInfo.append("插入customerAgent关系出错");
+								}else{
+									//循环权限
+									String[] roles=req.getRoles().split("\\,");
+									int sign=0;
+									for(int i=0;i<roles.length;i++){
+										req.setRoleId(Integer.parseInt(roles[i].toString()));
+										int temp4=customerManageMapper.getCusRoleInfo(req);
+										if(temp4>=1){
+											sign=1;
+											resultInfo.setLength(0);
+											resultInfo.append("已存在该用户与所选权限的关联关系");
+										}else{
+											int temp5=customerManageMapper.creCusRoleRelation(req);
+											if(temp5<1){
+												sign=1;
+												resultInfo.setLength(0);
+												resultInfo.append("插入该用户权限的关联关系出错");
+											}else{
+												resultInfo.setLength(0);
+												resultInfo.append("插入该用户权限的关联关系成功");
+											}
+										}
+									}
+									
+									if(sign==0){
+										resultCode=Response.SUCCESS_CODE;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		String result="代理商新增用户操作，结果为"+resultInfo.toString();
+		sys.operateRecord(result,req.getAgentsId());
+		map.put("resultCode", resultCode);
+		map.put("resultInfo", resultInfo.toString());
+		
+		return map;
+	}
+
+	/**
+	 * 单个删除
+	 * @param req
+	 * @return
+	 */
+	@Transactional(value="transactionManager-zhangfu",propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public Map<String, Object> deleteOne(CustomerManageReq req){
+		Map<String, Object> map=new HashMap<String, Object>();
+		int resultCode=Response.ERROR_CODE;
+		StringBuilder resultInfo=new StringBuilder();
+		int temp=customerManageMapper.delCustomer(req);
+		if(temp<1){
+			resultInfo.setLength(0);
+			resultInfo.append("删除customers表出错");
+		}else{
+			int temp1=customerManageMapper.delCusAgentRel(req);
+			if(temp1<1){
+				resultInfo.setLength(0);
+				resultInfo.append("删除customer_agent_relations表出错");
+			}else{
+				int temp2=customerManageMapper.delCusRoleRel(req);
+				if(temp2<1){
+					resultInfo.setLength(0);
+					resultInfo.append("删除customer_role_relations表出错");
+				}else{
+					resultCode=Response.SUCCESS_CODE;
+					resultInfo.setLength(0);
+					resultInfo.append("删除用户成功");
+				}
+			}
+		}
+		
+		String result="代理商删除用户操作，结果为"+resultInfo.toString();
+		sys.operateRecord(result,req.getAgentsId());
+		map.put("resultCode", resultCode);
+		map.put("resultInfo", resultInfo.toString());
+		return map;
+	}
+
+	/**
+	 * 批量删除
+	 * @param req
+	 * @return
+	 */
+	@Transactional(value="transactionManager-zhangfu",propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public Map<String,Object> deleteAll(CustomerManageReq req){
+		Map<String, Object> map=new HashMap<String, Object>();
+		int resultCode=Response.ERROR_CODE;
+		StringBuilder resultInfo=new StringBuilder();
+		
+		String[] customerIds=req.getCustomerIds().split("\\,");
+		for(int i=0;i<customerIds.length;i++){
+			req.setCustomerId(Integer.parseInt(customerIds[i].toString()));
+			
+			int temp=customerManageMapper.delCustomer(req);
+			if(temp<1){
+				resultCode=Response.ERROR_CODE;
+				resultInfo.setLength(0);
+				resultInfo.append("删除customers表出错");
+				break;
+			}else{
+				int temp1=customerManageMapper.delCusAgentRel(req);
+				if(temp1<1){
+					resultCode=Response.ERROR_CODE;
+					resultInfo.setLength(0);
+					resultInfo.append("删除customer_agent_relations表出错");
+					break;
+				}else{
+					int temp2=customerManageMapper.delCusRoleRel(req);
+					if(temp2<1){
+						resultCode=Response.ERROR_CODE;
+						resultInfo.setLength(0);
+						resultInfo.append("删除customer_role_relations表出错");
+						break;
+					}else{
+						resultCode=Response.SUCCESS_CODE;
+						resultInfo.setLength(0);
+						resultInfo.append("删除用户成功");
+					}
+				}
+			}
+			
+		}
+		
+		String result="代理商批量删除用户操作，结果为"+resultInfo.toString();
+		sys.operateRecord(result,req.getAgentsId());
+		map.put("resultCode", resultCode);
+		map.put("resultInfo", resultInfo.toString());
+		return map;
+	}
+	
+	
+	/**
+	 * 获取用户详细信息
+	 * @param req
+	 * @return
+	 */
+	public Map<String, Object> getInfo(CustomerManageReq req){
+		Map<String, Object> map=new HashMap<String, Object>();
+		int resultCode=Response.SUCCESS_CODE;
+		StringBuilder resultInfo=new StringBuilder();
+		List<Map<String, Object>> list=customerManageMapper.getInfo(req);
+		
+		if(list!=null && list.size()>0){
+			String loginId=list.get(0).get("username").toString();
+			String name=list.get(0).get("name").toString();
+			StringBuilder rolesStr=new StringBuilder();
+			for(int i=0;i<list.size();i++){
+				String roleId=list.get(i).get("role_id").toString();
+				if(rolesStr.length()<1){
+					rolesStr.append(roleId);
+				}else{
+					rolesStr.append(","+roleId);
+				}
+			}
+			Map<String, Object> result=new HashMap<String, Object>();
+			result.put("loginId", loginId);
+			result.put("name", name);
+			result.put("rolesStr", rolesStr);
+			map.put("result", result);
+			resultInfo.setLength(0);
+			resultInfo.append("成功");
+		}else{
+			resultInfo.setLength(0);
+			resultInfo.append("不存在数据");
+		}
+		map.put("resultCode", resultCode);
+		return map;	
+	}
+	
 }

b1b75553f2a69074942e34eb8ba4c92c8215cca6
jwb445875 <445875775@qq.com>
2015-04-24 13:55:29 +0800

    路径配置

--- src/main/java/com/comdosoft/financial/user/utils/alipayApp/config/AlipayConfig.java
+++ src/main/java/com/comdosoft/financial/user/utils/alipayApp/config/AlipayConfig.java
@@ -1,5 +1,7 @@
 package com.comdosoft.financial.user.utils.alipayApp.config;
 
+import com.comdosoft.financial.user.utils.RootUrl;
+
 /* *
  *类名：AlipayConfig
  *功能：基础配置类
@@ -32,8 +34,7 @@ public class AlipayConfig {
 	// 支付宝的公钥，无需修改该值
 	public static String ali_public_key  = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
 
-	public static String  backurl="http://121.40.84.2:28080/";
-    //public static String  backurl="http://agent.ebank007.com/";
+	public static String  backurl=RootUrl.urlpath;;
 
 	//↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 	

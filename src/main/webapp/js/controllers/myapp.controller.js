3348bb6896507cdd5c1e85650a85ed8a0bf40e73
vince.gao <vince.gao@hotmail.com>
2015-05-08 09:47:55 +0800

    解决左侧关闭展示

--- src/main/webapp/js/controllers/myapp.controller.js
+++ src/main/webapp/js/controllers/myapp.controller.js
@@ -10,6 +10,27 @@ var myappController = function ($scope, $http,$location, LoginService) {
 	}else{
 		//显示用户登录部分
 		$scope.$emit('changeshow',false);
+		
+		//左侧样式调整
+		$("#left_common li").unbind("click").bind("click", function(){
+			$(this).children('a').addClass("hover");
+			$(this).siblings().children('a').removeClass("hover");
+			if (!$(this).hasClass("second") ){ //判断是否有子节点
+				if ( !$(this).parents().hasClass("second") ){
+					$(".second").children('ol').children('li').children('a').removeClass("hover");
+				}
+			}
+	   });
+		
+		/*------用户后台导航菜单--------*/
+		$("li.second > a").click(function(){
+			$(this).parent().find("ol").toggle();
+			if(!$(this).parent().find("ol").is(":visible")){
+				$(this).find("i").removeClass("on").addClass("off");
+			}else{
+				$(this).find("i").removeClass("off").addClass("on");
+			}
+		});
 	}
 
 	$scope.my_message_list = function(){

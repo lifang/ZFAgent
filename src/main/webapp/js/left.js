926f3353795e7737ace2801302d6c1202b2df623
vince.gao <vince.gao@hotmail.com>
2015-05-09 21:20:43 +0800

    left

--- src/main/webapp/js/left.js
+++ src/main/webapp/js/left.js
@@ -1,26 +1,28 @@
 
 //input focus所有input焦点效果
 $(function(){
-	 
-	//左侧样式调整
-	$("#left_common li").unbind("click").bind("click", function(){
-		$(this).children('a').addClass("hover");
-		$(this).siblings().children('a').removeClass("hover");
-		if (!$(this).hasClass("second") ){ //判断是否有子节点
-			if ( !$(this).parents().hasClass("second") ){
-				$(".second").children('ol').children('li').children('a').removeClass("hover");
-			}
-		}
-   });
-	
+
 	/*------用户后台导航菜单--------*/
-	$("li.second > a:nth-child(1)").click(function(){
-		$(this).parent().find("ol").toggle();
-		if(!$(this).parent().find("ol").is(":visible")){
-			$(this).find("i").removeClass("on").addClass("off");
-		}else{
-			$(this).find("i").removeClass("off").addClass("on");
-		}
-	});
+	$(function(){
+		$(".side_menu > li >a:nth-child(1)").unbind("click").bind("click",function(){
+			$(this).parent().find("ol.sub_menu").toggle();
+			
+			$(this).parent().addClass("hover").siblings().removeClass("hover");
+			$(this).parent().siblings().find("ol.sub_menu li").removeClass("hover");
+			
+			if(!$(this).parent().find("ol.sub_menu").is(":visible")){
+				$(this).find("i").removeClass("on").addClass("off");
+			}else{
+				$(this).find("i").removeClass("off").addClass("on");
+			}
+		})
+		
+		$("ol.sub_menu >li > a").unbind("click").bind("click",function(){
+			$(this).parent("ol.sub_menu >li").addClass("hover").siblings().removeClass("hover");
+			$(this).parents(".side_menu >li").addClass("hover").siblings().removeClass("hover");
+			$(this).parents(".side_menu >li").siblings().find("ol.sub_menu >li").removeClass("hover");
+		});
+	})
+
  
 })

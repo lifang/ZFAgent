114686c27f371770b6b4b9eaaed5c69bed264e14
jwb445875 <445875775@qq.com>
2015-05-10 15:05:40 +0800

    1

--- src/main/webapp/js/controllers/shop.controller.js
+++ src/main/webapp/js/controllers/shop.controller.js
@@ -1188,7 +1188,10 @@ var shopmakeorderController = function($scope, $http, $location, LoginService) {
 		$scope.user.cityid = Math.ceil($scope.user.cityid);
 		$http.post("api/user/addCustomer", $scope.user).success(function(data) {
 			if (data.code == 1) {
-				$scope.clist();
+				$scope.cuslist=[];
+				$scope.selected="";
+				$scope.user={};
+				$scope.cuslist.push(data.result);
 			} else if (data.code == -1) {
 				alert(data.message);
 			}

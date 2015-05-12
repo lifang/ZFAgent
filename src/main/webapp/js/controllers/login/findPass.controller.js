7d78a39faceb62aa2f816accf98ee5441f2d2383
鲜飞虎 <531950712@qq.com>
2015-05-12 10:56:42 +0800

    找回密码修改

--- src/main/webapp/js/controllers/login/findPass.controller.js
+++ src/main/webapp/js/controllers/login/findPass.controller.js
@@ -6,6 +6,21 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 	var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
 	//手机格式
 	var reg = /^0?1[3|4|5|8][0-9]\d{8}$/;
+	//手机边框错误显示/消息
+	$scope.usernameinputboolean = false;
+	$scope.usernamemessageboolean = false;
+	$scope.codeinputboolean = false;
+	$scope.codeboolean = false;
+	
+	$scope.passinputone = false;
+	$scope.passinputtwo = false;
+	$scope.passmessagecoolean = false;
+	
+	$scope.successpass = false;
+	//图片校验码
+	$scope.imgcodeboolean = false;
+	//手机成功注册
+	$scope.gotosuccess = false;
 	//隐藏想邮箱发送邮件状态
 	$scope.songToEmail = false;
 	$scope.intDiff = 120;
@@ -46,33 +61,42 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 	};
 	//从新获得验证码
 	$scope.codeStatus = false;
+	$scope.sendnumber = 0;
 	$scope.newCode = function(){
 		if($scope.codeStatus == true){
 			$scope.codeStatus = false;
-			$http.post("api/agent/sendPhoneVerificationCode", {
-				codeNumber : $scope.phone_email,
-			}).success(function(data) {
-				if (data.code == 1) {
-					window.clearInterval(window.a);
-					window.clearInterval(window.b);
-					$scope.code = data.result;
-					$scope.codeNumber = "";
-					$scope.intDiff = 119;
-					window.b = window.setInterval(function(){
-				    	if($scope.intDiff == 0){
-				    		$('#day_show').html("点击获得验证码！");
-				    		$scope.codeStatus = true;
-				    		window.clearInterval(window.b);
-				    	}else{
-				    		$('#day_show').html("重新发送验证码（"+$scope.intDiff+"秒）");
-				    	    $scope.intDiff--;
-				    	}
-				    }, 1000);
-					intervalThree;
-				} else {
-					alert(data.message);
-				}
-			})
+			if($scope.sendnumber == 1){
+				$scope.$scope.imgcode;
+				$scope.reGetRandCodeImg();
+				$(".mask").css('display','block');
+				$(".re_alert_tab").css('display','block');
+			}else{
+				$http.post("api/agent/sendPhoneVerificationCode", {
+					codeNumber : $scope.phone_email,
+				}).success(function(data) {
+					if (data.code == 4) {
+						$scope.sendnumber = $scope.sendnumber + 1;
+						window.clearInterval(window.a);
+						window.clearInterval(window.b);
+						$scope.code = data.result;
+						$scope.codeNumber = "";
+						$scope.intDiff = 119;
+						window.b = window.setInterval(function(){
+					    	if($scope.intDiff == 0){
+					    		$('#day_show').html("点击获得验证码！");
+					    		$scope.codeStatus = true;
+					    		window.clearInterval(window.b);
+					    	}else{
+					    		$('#day_show').html("重新发送验证码（"+$scope.intDiff+"秒）");
+					    	    $scope.intDiff--;
+					    	}
+					    }, 1000);
+						intervalThree;
+					} else {
+						alert(data.message);
+					}
+				})
+			}
 		}
 	}
 	
@@ -81,9 +105,12 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 	
 	// 找回密码第一步
 	$scope.findPassOnes = function() {
+		//$scope.twostep();
+		
 		$scope.intDiff = 120;
 		if(!reg.test($scope.phone_email)&&!myreg.test($scope.phone_email)){
-			alert("请输入正确的手机/邮箱号码！");
+			$scope.usernameinputboolean = true;
+			$scope.usernamemessageboolean = true;
 		}else{
 			$http.post("api/agent/getFindUser", {
 				username : $scope.phone_email
@@ -91,13 +118,6 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 				if (data.code == -1) {//检验账号存在
 					alert(data.message);
 				} else {
-					$http.post("api/agent/sizeUpImgCode", {
-						imgnum : $scope.code
-					}).success(function(data) {
-						if (data.code == -1) {
-							alert(data.message);
-						} else {
-							// 下一步
 							if (myreg.test($scope.phone_email)) {
 								// 向邮箱发送密码重置邮件！
 								$http.post("api/agent/sendEmailVerificationCode", {
@@ -136,8 +156,7 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 									}
 								})
 							}
-						}
-					})
+				
 				}
 			})
 		}
@@ -145,6 +164,8 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 	
 	// 找回密码第三步
 	$scope.findPassThree = function() {
+		//$scope.threestep();
+		
 		if($scope.code == $scope.codeNumber){
 			$http.post("api/agent/webFicationCode", {
 			code : $scope.codeNumber,
@@ -153,33 +174,76 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 			if (data.code == 1) {
 				$scope.threestep();
 			} else if (data.code == -1) {
-				alert(data.message);
+				$scope.codeinputboolean = true;
+				$scope.codeboolean = true;
 			}
 			})
 		}else{
-			alert("验证码错误！");
+			$scope.codeinputboolean = true;
+			$scope.codeboolean = true;
 		}
 	};
 	
+	//键盘事件
+	$scope.keyupdownpas1 = function(){
+		if($scope.password1==''||$scope.password1==null){
+			$scope.passinputone = true;
+		}else if ($scope.password1.length<6||$scope.password1.length>20) {
+			$scope.passinputone = true;
+		}else{
+			$scope.passinputone = false;
+			$scope.successpass = true;
+		}
+	}
+	$scope.keyupdownpas2 = function(){
+		if($scope.password2==''||$scope.password2==null){
+			$scope.passinputtwo = true;
+		}else if ($scope.password2.length<6||$scope.password2.length>20) {
+			$scope.passinputtwo = true;
+		}else if ($scope.password1 != $scope.password2) {
+			$scope.passinputtwo = true;
+			$scope.passmessagecoolean = true;
+		}else{
+			$scope.passinputtwo = false;
+			$scope.passmessagecoolean = false;
+		}
+	}
+	
+	//跳转邮箱登陆
+	$scope.gotoemail = function(){
+		var t=$scope.phone_email.lastIndexOf('@')+1;
+		var str = "http://mail."+$scope.phone_email.substring(t);
+		window.location.href=str;
+	}
+	
 	// 开始找回(手机)
 	$scope.findPassEnd = function() {
-		if($scope.password1==''||$scope.password1==null||$scope.password2==''||$scope.password2==null){
-			alert("密码不能为空！");
-		}else if ($scope.password1.length<6||$scope.password1.length>20||$scope.password2.length<6||$scope.password2.length>20) {
-			alert("密码由6-20位，英文字符组成！");
-		}else if ($scope.password1 != $scope.password2) {
-			alert("密码不一致！");
-		} else {
-			$http.post("api/agent/webUpdatePass", {
-				password : $scope.password1,
-				username : $scope.phone_email
-			}).success(function(data) {
-				if (data.code == 1) {
-					 window.location.href = '#/login';
-				} else if (data.code == -1) {
-					alert(data.message);
-				}
-			})
+		if($scope.password1==''||$scope.password1==null){
+			$scope.passinputone = true;
+		}else if ($scope.password1.length<6||$scope.password1.length>20) {
+			$scope.passinputone = true;
+		}else{
+			$scope.successpass = true;
+			if($scope.password2==''||$scope.password2==null){
+				$scope.passinputtwo = true;
+			}else if ($scope.password2.length<6||$scope.password2.length>20) {
+				$scope.passinputtwo = true;
+			}else if ($scope.password1 != $scope.password2) {
+				$scope.passinputtwo = true;
+				$scope.passmessagecoolean = true;
+			} else {
+				$http.post("api/agent/webUpdatePass", {
+					password : $scope.password1,
+					username : $scope.phone_email
+				}).success(function(data) {
+					if (data.code == 1) {
+						$scope.gotosuccess = true;
+						// window.location.href = '#/login';
+					} else if (data.code == -1) {
+						alert(data.message);
+					}
+				})
+			}
 		}
 	};
 	// 开始找回(邮箱)
@@ -203,6 +267,24 @@ var findpassController=function($scope, $location, $http, LoginService,$timeout)
 			})
 		}
 	};
+	
+	//第二步校验图片验证码
+	$scope.judgecode = function(){
+		$http.post("api/agent/sizeUpImgCode", {
+			imgnum : $scope.imgcode
+		}).success(function(data) {
+			if (data.code == -1) {
+				$scope.imgcodeboolean = true;
+				$scope.reGetRandCodeImg();
+			} else {
+					$scope.codeStatus = true;
+					$scope.sendnumber = 0;
+					$(".mask").css('display','none');
+					$(".re_alert_tab").css('display','none');
+					$scope.newCode();
+			}
+		});
+	}
 	
 	$scope.reGetRandCodeImg();
 	$scope.init();
